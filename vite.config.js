0ba2d6c19b4228cc134db251343df46256acf56a
cp985 <166384434+cp985@users.noreply.github.com>
2026-01-31 18:56:07 +0100

    Fix: actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Il base deve corrispondere esattamente al nome della tua repository su GitHub
+  base: '/react-redux-shopCv/',
 })
