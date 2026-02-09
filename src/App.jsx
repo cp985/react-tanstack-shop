31ed9ecc84d10cc2932ad409c25e49cac8492754
cp985 <166384434+cp985@users.noreply.github.com>
2026-02-09 18:47:19 +0100

    fix try favico e flash

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ErrorPage from "./components/general/ErrorPage";
 import WrapperLayoutMainNav from "./components/general/WrapperLayoutMainNav";
 import MainPageShop from "./pages/MainPageShop";
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   [
     {
       path: "/",
@@ -40,10 +40,10 @@ const router = createHashRouter(
         },
       ],
     }
-  ]
-  // {
-  //   basename: "/react-tanstack-shop/",
-  // },
+  ],
+  {
+    basename: "/react-tanstack-shop/",
+  }
 );
 
 function App() {
