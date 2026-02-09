0d2c85de03a9e6eb861c81d2464f9854d31bdea4
cp985 <166384434+cp985@users.noreply.github.com>
2026-02-09 14:22:07 +0100

    routes and schelethon pages  init

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Cart from "./pages/Cart";
 import NewsLetterContact from "./pages/NewsletterContact";
 import AccountUser from "./pages/AccountUser";
 import ErrorPage from "./components/general/ErrorPage";
-import WrapperLayoutMainNav from "./components/general/wrapperLayoutMainNav";
+import WrapperLayoutMainNav from "./components/general/WrapperLayoutMainNav";
 
 const router = createBrowserRouter(
   [
