cfdde47285b99cb8d99a436f5f4a06e16b4ac0a5
cp985 <166384434+cp985@users.noreply.github.com>
2026-03-03 11:23:42 +0100

    work on formfilter ui

--- src/components/UI/FormFilter.jsx
+++ src/components/UI/FormFilter.jsx
@@ -6,7 +6,6 @@ export default function FormFilter() {
   const { formData, setFormData } = useItems();
   let path = useLocation().pathname;
 
-
   function handlerStatusChange(e) {
     const { name, value, checked, type } = e.target;
 
@@ -28,207 +27,220 @@ export default function FormFilter() {
 
   return (
     <form className={classFormFilter["form-filter"]}>
-      <fieldset>
-        <legend>Ordina per</legend>
-        <select name="sortBy" onChange={handlerStatusChange}>
-          <option value="prezzo">Prezzo</option>
-          <option value="nome">Nome</option>
-          <option value="rarita">Rarità</option>
-        </select>
+      <details>
+        <summary>Ordina per</summary>
 
-        <select name="sortOrder" onChange={handlerStatusChange}>
-          <option value="asc">Crescente</option>
-          <option value="desc">Decrescente</option>
-        </select>
-        {path === "/app/shop" && (<Input
-          type="checkbox"
-          id="onSale"
-          name="onSale"
-          value={"onSale"}
-          label="On Sale"
-          hidden
-          onChange={handlerStatusChange}
-        />)}
-      </fieldset>
+        <div className={classFormFilter["details-container"]}>
+          <select name="sortBy" onChange={handlerStatusChange}>
+            <option value="prezzo">Prezzo</option>
+            <option value="nome">Nome</option>
+            <option value="rarita">Rarità</option>
+          </select>
 
-      <fieldset>
-        <legend>Rarità</legend>
-        <Input
-          classOf="input-filter"
-          type="checkbox"
-          id="comune"
-          name="rarita"
-          label="Comune"
-          value="Comune"
-          onChange={handlerStatusChange}
-          hidden
-        />
-        <Input
-          classOf="input-filter"
-          type="checkbox"
-          id="rara"
-          name="rarita"
-          label="Rara"
-          value="Rara"
-          onChange={handlerStatusChange}
-          hidden
-        />
-        <Input
-          classOfInput="input-filter"
-          classOfLabel={"label-filter"}
-          type="checkbox"
-          id="leggendaria"
-          name="rarita"
-          label="Leggendaria"
-          value="Leggendaria"
-          onChange={handlerStatusChange}
-          hidden
-        />
-        <Input
-          classOfInput="input-filter"
-          classOfLabel={"label-filter"}
-          type="checkbox"
-          id="epica"
-          name="rarita"
-          label="Epica"
-          value="Epica"
-          onChange={handlerStatusChange}
-          hidden
-        />
-      </fieldset>
+          <select name="sortOrder" onChange={handlerStatusChange}>
+            <option value="asc">Crescente</option>
+            <option value="desc">Decrescente</option>
+          </select>
+          {path === "/app/shop" && (
+            <Input
+              type="checkbox"
+              id="onSale"
+              name="onSale"
+              value={"onSale"}
+              label="On Sale"
+              hidden
+              onChange={handlerStatusChange}
+            />
+          )}
+        </div>
+      </details>
 
-      <fieldset>
-        <legend>Tipo</legend>
-        <Input
-          classOfInput="input-filter"
-          classOfLabel={"label-filter"}
-          type="checkbox"
-          id="arma"
-          name="categoria"
-          label="Arma"
-          value="Arma"
-          onChange={handlerStatusChange}
-          hidden
-        />
-        <Input
-          classOfInput="input-filter"
-          classOfLabel={"label-filter"}
-          type="checkbox"
-          id="armatura"
-          name="categoria"
-          label="Armatura"
-          value="Armatura"
-          onChange={handlerStatusChange}
-          hidden
-        />
+      <details>
+        <summary>Rarità</summary>
 
-        <Input
-          classOfInput="input-filter"
-          classOfLabel={"label-filter"}
-          type="checkbox"
-          id="consumabile"
-          name="categoria"
-          label="Consumabile"
-          value="Consumabile"
-          onChange={handlerStatusChange}
-          hidden
-        />
-      </fieldset>
+        <div className={classFormFilter["details-container"]}>
+          <Input
+            classOf="input-filter"
+            type="checkbox"
+            id="comune"
+            name="rarita"
+            label="Comune"
+            value="Comune"
+            onChange={handlerStatusChange}
+            hidden
+          />
+          <Input
+            classOf="input-filter"
+            type="checkbox"
+            id="rara"
+            name="rarita"
+            label="Rara"
+            value="Rara"
+            onChange={handlerStatusChange}
+            hidden
+          />
+          <Input
+            classOfInput="input-filter"
+            classOfLabel={"label-filter"}
+            type="checkbox"
+            id="leggendaria"
+            name="rarita"
+            label="Leggendaria"
+            value="Leggendaria"
+            onChange={handlerStatusChange}
+            hidden
+          />
+          <Input
+            classOfInput="input-filter"
+            classOfLabel={"label-filter"}
+            type="checkbox"
+            id="epica"
+            name="rarita"
+            label="Epica"
+            value="Epica"
+            onChange={handlerStatusChange}
+            hidden
+          />
+        </div>
+      </details>
 
-      <fieldset>
-        <legend>Classe</legend>
-        <Input
-          classOfInput=" input-filter"
-          classOfLabel={"label-filter"}
-          type="checkbox"
-          id="mago"
-          name="classe"
-          label="Mago"
-          value="Mago"
-          onChange={handlerStatusChange}
-          hidden
-        />
-        <Input
-          classOf=" input-filter"
-          type="checkbox"
-          id="guerriero"
-          name="classe"
-          label="Guerriero"
-          value="Guerriero"
-          onChange={handlerStatusChange}
-          hidden
-        />
-        <Input
-          classOf=" input-filter"
-          type="checkbox"
-          id="assassino"
-          name="classe"
-          label="Assassino"
-          value="Assassino"
-          onChange={handlerStatusChange}
-          hidden
-        />
+      <details>
+        <summary>Tipo</summary>
+        <div className={classFormFilter["details-container"]}>
+          <Input
+            classOfInput="input-filter"
+            classOfLabel={"label-filter"}
+            type="checkbox"
+            id="arma"
+            name="categoria"
+            label="Arma"
+            value="Arma"
+            onChange={handlerStatusChange}
+            hidden
+          />
+          <Input
+            classOfInput="input-filter"
+            classOfLabel={"label-filter"}
+            type="checkbox"
+            id="armatura"
+            name="categoria"
+            label="Armatura"
+            value="Armatura"
+            onChange={handlerStatusChange}
+            hidden
+          />
 
-        <Input
-          classOf=" input-filter"
-          type="checkbox"
-          id="paladino"
-          name="classe"
-          label="Paladino"
-          value="Paladino"
-          onChange={handlerStatusChange}
-          hidden
-        />
+          <Input
+            classOfInput="input-filter"
+            classOfLabel={"label-filter"}
+            type="checkbox"
+            id="consumabile"
+            name="categoria"
+            label="Consumabile"
+            value="Consumabile"
+            onChange={handlerStatusChange}
+            hidden
+          />
+        </div>
+      </details>
 
-        <Input
-          classOf=" input-filter"
-          type="checkbox"
-          id="druido"
-          name="classe"
-          label="Druido"
-          value="Druido"
-          onChange={handlerStatusChange}
-          hidden
-        />
+      <details>
+        <summary>Classe</summary>
+        <div className={classFormFilter["details-container"]}>
+          <Input
+            classOfInput=" input-filter"
+            classOfLabel={"label-filter"}
+            type="checkbox"
+            id="mago"
+            name="classe"
+            label="Mago"
+            value="Mago"
+            onChange={handlerStatusChange}
+            hidden
+          />
+          <Input
+            classOf=" input-filter"
+            type="checkbox"
+            id="guerriero"
+            name="classe"
+            label="Guerriero"
+            value="Guerriero"
+            onChange={handlerStatusChange}
+            hidden
+          />
+          <Input
+            classOf=" input-filter"
+            type="checkbox"
+            id="assassino"
+            name="classe"
+            label="Assassino"
+            value="Assassino"
+            onChange={handlerStatusChange}
+            hidden
+          />
 
-        <Input
-          classOf=" input-filter"
-          type="checkbox"
-          id="cacciatore"
-          name="classe"
-          label="Cacciatore"
-          value="Cacciatore"
-          onChange={handlerStatusChange}
-          hidden
-        />
-      </fieldset>
-      <fieldset className={classFormFilter["fieldset-prezzo"]}>
-        <legend>Prezzo</legend>
+          <Input
+            classOf=" input-filter"
+            type="checkbox"
+            id="paladino"
+            name="classe"
+            label="Paladino"
+            value="Paladino"
+            onChange={handlerStatusChange}
+            hidden
+          />
 
-        <Input
-          type="range"
-          id="prezzoMin"
-          name="prezzoMin"
-          min="0"
-          max="10000"
-          step="100"
-          onChange={handlerStatusChange}
-          value={formData.prezzoMin}
-          label={`Prezzo Min: ${formData.prezzoMin} monete`}
-        />
+          <Input
+            classOf=" input-filter"
+            type="checkbox"
+            id="druido"
+            name="classe"
+            label="Druido"
+            value="Druido"
+            onChange={handlerStatusChange}
+            hidden
+          />
 
-        <Input
-          type="range"
-          id="prezzoMax"
-          name="prezzoMax"
-          min="0"
-          max="10000"
-          step="100"
-          onChange={handlerStatusChange}
-          value={formData.prezzoMax}
-          label={`Prezzo Max: ${formData.prezzoMax} monete`}
-        />
-      </fieldset>
+          <Input
+            classOf=" input-filter"
+            type="checkbox"
+            id="cacciatore"
+            name="classe"
+            label="Cacciatore"
+            value="Cacciatore"
+            onChange={handlerStatusChange}
+            hidden
+          />
+        </div>
+      </details>
+      <details className={classFormFilter["fieldset-prezzo"]}>
+        <summary>Prezzo</summary>
+        <div className={classFormFilter["details-container"]}>
+          <Input
+            type="range"
+            id="prezzoMin"
+            name="prezzoMin"
+            min="0"
+            max="10000"
+            step="100"
+            onChange={handlerStatusChange}
+            value={formData.prezzoMin}
+            label={`Prezzo Min: ${formData.prezzoMin} monete`}
+          />
+
+          <Input
+            type="range"
+            id="prezzoMax"
+            name="prezzoMax"
+            min="0"
+            max="10000"
+            step="100"
+            onChange={handlerStatusChange}
+            value={formData.prezzoMax}
+            label={`Prezzo Max: ${formData.prezzoMax} monete`}
+          />
+        </div>
+      </details>
     </form>
   );
 }
